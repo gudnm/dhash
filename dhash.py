a4c8e939b271415bc30b43a8dd41727ca376dde4
gudnm <srochno@gmail.com>
2016-09-06 11:00:36 -0400

    Adding features and custom hash functions

--- dhash.py
+++ dhash.py
@@ -21,45 +21,51 @@ import string
 import threading
 
 class DHash(object):
-    def __init__(self, nodes, config={}):
+    def __init__(self, nodes, config=None):
+        self.nodes = nodes
         if not config:
             config = {
                 'resizing_method': ConsistentHashing,
                 'access_pattern': WriteAround,
                 'evection_strategy': LFU
             }
-        self.resizer = config['resizing_method']()
+        if not hasattr(config['resizing_method'], 'get_nodeid'):
+            raise SystemError("Oh no!")
+        else:
+            self.resizer = config['resizing_method'](self.nodes)
         self.accessor = config['access_pattern']()
-        self.nodes = nodes
 
     def read(self, key):
-        nodeid = self.resizer.get_nodeid()
-        return self.nodes[nodeid].read(key)
+        nodeid = self.resizer.get_nodeid(key)
+        return self.nodes[nodeid].read(self.accessor, key)
 
     def write(self, key, value):
-        nodeid = self.resizer.get_nodeid()
-        self.nodes[nodeid].write(key, value)
+        nodeid = self.resizer.get_nodeid(key)
+        self.nodes[nodeid].write(self.accessor, key, value)
 
     def add_node(self, node):
         self.nodes.append(node)
+        self.resizer.resize()
 
     def remove_node(self, node):
         del self.nodes[node]
+        self.resizer.resize()
 
 class MockNode(object):
     """Implement a node. 
 
     Node will run in its own thread and emulate a separate machine.
     """
-    def __init__(self, size=128):
+    def __init__(self, name, size=128):
+        self.name = name
         self.thread = threading.Thread(target=self.run)
         self.size = size
         self.hashmap = {}
 
-    def read(self, key):
+    def read(self, accessor, key):
         return self.hashmap.get(key, None)
 
-    def write(self, key, value):
+    def write(self, accessor, key, value):
         self.hashmap[key] = value
 
     def run(self):
@@ -69,38 +75,90 @@ class MockDB(object):
     """Emulate database via file read/write."""
     pass
 
-class WriteThru(object):
+class Accessor(object):
+    """"""
+    pass
+
+class WriteThru(Accessor):
     """Implement write-thru access pattern."""
     pass
 
-class WriteAround(object):
+class WriteAround(Accessor):
     """Implement write-around access pattern."""
     pass
 
-class WriteBack(object):
+class WriteBack(Accessor):
     """Implement write-back access pattern."""
     pass
 
-class LRU(object):
-    """Implement Least-Recently-Used eviction strategy."""
-    pass
-
-class LFU(object):
-    """Implement Least-Frequently-Used eviction strategy."""
-    pass
-
-class ConsistentHashing(object):
-    """Implement a consistent hashing ring."""
+class Evictor(object):
     def __init__(self):
         pass
 
-    def get_nodeid(self):
+    def update_on_read(self):
+        pass
+
+    def remove_for_write(self):
+        pass
+
+class LRU(Evictor):
+    """Implement Least-Recently-Used eviction strategy."""
+    pass
+
+class LFU(Evictor):
+    """Implement Least-Frequently-Used eviction strategy."""
+    pass
+
+class FIFO(Evictor):
+    """Implement First-In-First-Out eviction strategy."""
+    pass
+
+class Resizer(object):
+
+    def get_nodeid(self, key):
+        raise NotImplementedError()
+
+    def resize(self):
+        raise NotImplementedError()
+
+    def hash1(self, node_name):
+        res = 0
+        for char in node_name:
+            res += 1723**ord(char)
+        return res % 2**64
+
+    def hash2(self, node_name):
+        res = 0
+        for char in node_name:
+            res += 1327**ord(char)
+        return res % 2**64
+
+class ConsistentHashing(Resizer):
+    """Implement a consistent hashing ring."""
+    def __init__(self, nodes):
+        self.positions = []
+        for node in nodes:
+            self.positions.append(self.hash1(node.name))
+            self.positions.append(self.hash2(node.name))
+
+    def get_nodeid(self, key):
         """For now, return 0."""
         return 0
 
-class RendezvousHashing(object):
+    def resize(self):
+        pass
+
+class RendezvousHashing(Resizer):
     """Implement Highest Random Weight hashing method."""
-    pass
+    def __init__(self):
+        pass
+
+    def get_nodeid(self, key):
+        """For now, return 0."""
+        return 0
+
+    def resize(self):
+        pass
 
 def dummy_key_value_pair():
     names = ['dan', 'ben', 'jim', 'joe']
@@ -111,10 +169,24 @@ def dummy_key():
     return random.choice(names)+str(random.randint(10,99))
 
 if __name__ == '__main__':
-    node1 = MockNode()
-    node2 = MockNode()
+    resizer = Resizer()
+    for _ in range(10):
+        key = dummy_key()
+        print(key, resizer.hash1(key), resizer.hash2(key))
+
+    node1 = MockNode('Machine 1')
+    assert resizer.hash1(node1.name) == 14738669757681942741
+    assert resizer.hash2(node1.name) == 5343253232099587805
+    assert hash(node1.name) == 3927025894220883735
+
+    node2 = MockNode('Machine 2')
+    config = {
+                'resizing_method': MockNode,
+                'access_pattern': WriteAround,
+                'evection_strategy': LFU
+            }
     dhash = DHash([node1, node2])
-    for _ in range(1000):
+    for _ in range(100):
         key, value = dummy_key_value_pair()
         dhash.write(key, value)
         if random.randint(0,5) == 0:
