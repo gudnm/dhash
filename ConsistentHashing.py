c8ae285712cd842d69014b8ca1ffa93510f1f5d9
gudnm <srochno@gmail.com>
2016-09-13 13:30:40 -0400

    Add some tests and move hashes method from Resizer to ConsistentHashing

--- ConsistentHashing.py
+++ ConsistentHashing.py
@@ -4,11 +4,39 @@ class ConsistentHashing(Resizer):
     """Implement a consistent hashing ring."""
     def __init__(self, nodes):
         super(ConsistentHashing, self).__init__()
+        self.hash_functions = [
+            self._hash0, 
+            self._hash1, 
+            self._hash2
+        ]
         self.positions = []
         for node in nodes:
             for somehash in self.hashes(node.name):
                 self.positions.append((somehash, node.id))
         self.positions.sort()
+
+    # I need to generate a bunch of new positions, for now I have 3 hash functions, including the built-in, and all nodes get 3 'locations' in the ring, eventually this needs to be variable where some nodes will get more or less depending on their capacity
+    def _custom_hash(self, node_name, prime):
+        res = 0
+        for char in node_name:
+            res += prime**ord(char)
+        res %= 2**63
+        return res if res % 10 < 5 else -res        
+
+    def _hash0(self, node_name):
+        return hash(node_name)
+
+    def _hash1(self, node_name):
+        return self._custom_hash(node_name, 1723)
+
+    def _hash2(self, node_name):
+        return self._custom_hash(node_name, 1327)
+
+    def hashes(self, node_name, num=3):
+        res = []
+        for i in range(min(len(self.hash_functions), num)):
+            res.append(self.hash_functions[i](node_name))
+        return res
 
     def get_nodeid(self, key):
         """Find the node to use for the given key's storage."""
--- test.py
+++ test.py
@@ -1,12 +1,30 @@
 import unittest
-from dhash import DHash
+from DHash import DHash
 from MockNode import MockNode
 from Resizer import Resizer
 from ConsistentHashing import ConsistentHashing
 
 class DHashTest(unittest.TestCase):
-	def testSomething():
+
+	def setUp(self):
+		self.node = MockNode('Node #0', 0)
+		self.dhash = DHash([self.node])
+		self.resizer = self.dhash.resizer
+
+	def testSomething(self):
 		pass
+
+class ResizerTest(unittest.TestCase):
+	"""docstring for ResizerTest"""
+
+	def setUp(self):
+		self.node = MockNode('Node #0', 0)
+		self.dhash = DHash([self.node])
+		self.resizer = self.dhash.resizer
+
+	def testHashes(self):
+		hashes = self.resizer.hashes(self.node.name)
+		self.assertEqual(len(hashes), 3)
 
 if __name__ == '__main__':
 	unittest.main()
